8c918ba27f63fa418f59ffe2452ad44d49ec3998
Gary Storey <garystorey655@gmail.com>
2026-02-25 09:31:47 -0500

    fic config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  tmdbApiKey: 'b878f255836a67e76ca1b65069462397',
   production: false,
+  tmdbApiKey: 'b878f255836a67e76ca1b65069462397',
   tmdbBaseUrl: 'https://api.themoviedb.org/3',
 };
